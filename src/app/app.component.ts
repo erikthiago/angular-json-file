5e82f8a9466fb7fce4dda330fbc0138caeff3e00
Erik Sousa <erik.sousa@quatto.com.br>
2019-06-05 15:37:42 -0300

    Projeto terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  result: any;
+  urlToJson = 'assets/exemploJson.json';
+
+  constructor(public http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.http.get<any>(this.urlToJson).subscribe(response => {
+      this.result = response;
+    });
+  }
+
+
   title = 'angular-json-file';
 }
